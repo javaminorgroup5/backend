fd16e5a6bd8909baca7b1dd79ba83d18b35ac026
Nathan <nathanernst@hotmail.com>
2021-01-23 12:32:58 +0100

    fix GroupControllerTest

--- src/main/java/nl/hro/cookbook/controller/GroupController.java
+++ src/main/java/nl/hro/cookbook/controller/GroupController.java
@@ -21,8 +21,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
