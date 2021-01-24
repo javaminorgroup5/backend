a556a3462a9dd409d10f345d5f4697eca4cd4288
Nathan <nathanernst@hotmail.com>
2021-01-24 13:46:21 +0100

    Removed properties already in AdminController

--- src/main/java/nl/hro/cookbook/controller/admin/category/CategoryController.java
+++ src/main/java/nl/hro/cookbook/controller/admin/category/CategoryController.java
@@ -13,37 +13,40 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@RequestMapping("/category")
 public class CategoryController extends AdminController {
 
     private final CategoryService categoryService;
     private final CategoryMapper categoryMapper;
 
-    @GetMapping("/category")
+    @GetMapping
     public Collection<CategoryDTO> getAllCategories() {
         return categoryService.findAllCategories().stream()
                 .map(categoryMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
-    @PostMapping("/category")
+    @PostMapping
     public ResponseEntity<?> createCategory(@RequestBody final CategoryDTO category) {
         Category categoryModel = categoryMapper.toModel(category);
         categoryService.saveCategory(categoryModel);
         return ResponseEntity.ok(categoryModel.getId());
     }
 
-    @PatchMapping("/category/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> editCategory(@PathVariable("id") final long id, @RequestBody final CategoryDTO category) {
         Category categoryModel = categoryMapper.toModel(category);
         categoryService.updateCategory(id, categoryModel);
         return ResponseEntity.ok(categoryModel.getId());
     }
 
-    @DeleteMapping("/category/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCategory(@PathVariable final long id) {
         categoryService.deleteCategoryById(id);
         return ResponseEntity.ok().build();
